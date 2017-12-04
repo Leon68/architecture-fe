3c7efd0fa1798fb60e3c57226b6676b2380dbc87
Leon <wanliangwang@163.com>
2017-12-04 21:49:16 +0800

    修复模块问题

--- dist/sale.js
+++ dist/sale.js
@@ -65,9 +65,12 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed: Error: Cannot find module 'babel-plugin-transform-es2015-arrow-functions' (While processing preset: \"/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-preset-es2015@6.24.1@babel-preset-es2015/lib/index.js\")\n    at Function.Module._resolveFilename (module.js:440:15)\n    at Function.Module._load (module.js:388:25)\n    at Module.require (module.js:468:17)\n    at require (internal/module.js:20:19)\n    at Object.<anonymous> (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-preset-es2015@6.24.1@babel-preset-es2015/lib/index.js:17:49)\n    at Module._compile (module.js:541:32)\n    at Object.Module._extensions..js (module.js:550:10)\n    at Module.load (module.js:458:32)\n    at tryModuleLoad (module.js:417:12)\n    at Function.Module._load (module.js:409:3)\n    at Module.require (module.js:468:17)\n    at require (internal/module.js:20:19)\n    at /home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/options/option-manager.js:296:17\n    at Array.map (native)\n    at OptionManager.resolvePresets (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/options/option-manager.js:275:20)\n    at OptionManager.mergePresets (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/options/option-manager.js:264:10)\n    at OptionManager.mergeOptions (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/options/option-manager.js:249:14)\n    at OptionManager.init (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/options/option-manager.js:368:12)\n    at File.initOptions (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/index.js:212:65)\n    at new File (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/file/index.js:135:24)\n    at Pipeline.transform (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-core@6.26.0@babel-core/lib/transformation/pipeline.js:46:16)\n    at transpile (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-loader@7.1.2@babel-loader/lib/index.js:50:20)\n    at Object.module.exports (/home/leon/Desktop/frontend/architecture-fe/node_modules/_babel-loader@7.1.2@babel-loader/lib/index.js:175:20)");
+"use strict";
+
+
+var a = 1;
 
 /***/ })
 /******/ ]);
