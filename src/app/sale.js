16980bfa29bc61db82824bfdafe928c9af2dbc51
Leon <wanliangwang@163.com>
2017-12-06 22:07:43 +0800

    jest单元测试

--- src/app/sale.js
+++ src/app/sale.js
@@ -1,4 +1,10 @@
 import './sale.css'
+import './style.scss'
+import React from 'react'
+import ReactDom from 'react-dom'
+
 let a = 1
 let c = 2
 let c2 = 2
+const node = document.getElementById('root')
+ReactDom.render("hello world2",node)
